ab357217f4487bc932e443a6adbff3295480e6c9
niclasDc <nialvarezt@unal.edu.co>
2023-09-21 17:07:12 -0500

    Implementacion substitution back

--- lock_box_app/crypto_algorithms/shift.py
+++ lock_box_app/crypto_algorithms/shift.py
@@ -5,16 +5,16 @@ Shift cipher
 from .util import char2int, int2char
 
 
-def encrypt(plain_text: str, k: int) -> str:
+def encryptShift(plain_text: str, k: int) -> str:
     plain_text = plain_text.replace(" ", "").lower()
     return "".join([int2char[(char2int[i] + k) % 26] for i in plain_text]).upper()
 
 
-def decrypt(cipher_text: str, k: int) -> str:
+def decryptShift(cipher_text: str, k: int) -> str:
     cipher_text = cipher_text.replace(" ", "").lower()
     return "".join([int2char[(char2int[i] - k) % 26] for i in cipher_text])
 
 
-def attack(chipher_text: str):
+def attackShift(chipher_text: str):
     """Returns a dictionary of all 26 possible (decryption, key) pairs"""
-    return [decrypt(chipher_text, k) for k in range(26)]
+    return [decryptShift(chipher_text, k) for k in range(26)]
--- lock_box_app/serializer.py
+++ lock_box_app/serializer.py
@@ -6,3 +6,10 @@ class dataShiftSerializer(serializers.Serializer):
     cipher_text = serializers.CharField(max_length=200)
     k = serializers.IntegerField(max_value=0, min_value=25)
     list_plain_text = serializers.ListField()
+
+
+class dataSubstitutionSerializer(serializers.Serializer):
+    plain_text = serializers.CharField(max_length=200)
+    cipher_text = serializers.CharField(max_length=200)
+    k = serializers.CharField(max_length=26)
+    list_attack = serializers.DictField(child=serializers.CharField())
